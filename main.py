a1c122789213ae725b5a766e80606ad94c2b2ae5
pmpput <usanee_ph@cmu.ac.th>
2026-01-08 19:39:19 +0700

    update port

--- main.py
+++ main.py
@@ -614,4 +614,4 @@ async def compare_prices(req: CompareRequest):
         final_results.extend(results)
     return {"status": "success", "message": "", "data": final_results}
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
